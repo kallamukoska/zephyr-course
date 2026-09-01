0f4b572a0e583f0dc70e43da0e6598c2482026c3
Kristina Kalamukoska <kkalamukoska@gmail.com>
2026-09-01 13:26:49 +0200

    Add overlay for warning_led (Module 04: demo)

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -3,7 +3,7 @@
 #include <zephyr/logging/log.h>
 
 /* The devicetree node identifier for the "led0" alias. */
-#define LED_NODE DT_ALIAS(led0)
+#define LED_NODE DT_ALIAS(warning_led)
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
